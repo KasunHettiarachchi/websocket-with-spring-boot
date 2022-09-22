e0fe8760a19ce42f99aac5db5413b4467edb5f3d
Kasun Sampath <kasun.sampath@cloudsolution-sa.com>
2022-09-22 12:51:36 +0530

    init commit

---
+++ src/main/java/com/project/websocket/WebsocketSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.project.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketSpringBootApplication.class, args);
+	}
+
+}
